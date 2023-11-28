39658e607cc81898059e2b921755d7713d251fc2
Santin04 <masantinte@gmail.com>
2023-11-28 10:06:12 -0300

    create cylinder, palne and sphere

---
+++ scenes/scene04.js
@@ -0,0 +1,51 @@
+//criando o material que os objetos vão usar
+const material = new THREE.MeshLambertMaterial(
+    {color: 0x348feb}
+)
+
+
+//criando um cilindro
+const cylinder = new THREE.Mesh(
+    new THREE.CylinderBufferGeometry(
+        //raio de cima, raio de baixo e altura
+        0.5, 0.5, 1
+    ),
+    material
+)
+scene.add(cylinder)
+
+
+//criando um plano (parede/chãos)
+const plane = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(
+        //raio de cima, raio de baixo e altura
+        1.5, 1.5,
+    ),
+    material
+)
+plane.position.x = 2
+scene.add(plane)
+//para usa-lo como chão, bosta rotacionar ele 90°
+
+
+//criando uma esfera
+const sphere = new THREE.Mesh(
+    new THREE.SphereBufferGeometry(
+        //raio, qntd de lado na largura, qntd de lados na altura
+        //quanto maior o valor do segundo e terceiro item, mais redondo fica
+        0.5, 15, 15
+    ),
+    material
+)
+sphere.position.x = -2
+scene.add(sphere)
+
+renderer.setAnimationLoop(() => {
+
+    x3.tick()
+
+    x3.fps(()=>{
+        renderer.render(scene, camera)
+    })
+    
+})
